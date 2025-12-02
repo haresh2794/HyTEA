fcc8f228aa66794735da5d1489a23b855a77d70d
haresh <hareshmvc@gmail.com>
2025-12-02 01:57:16 +0000

    RenewableElectricity Class tested directly on Colab and confirmed

--- hytea/hytea_components/rese/rese_model.py
+++ hytea/hytea_components/rese/rese_model.py
@@ -15,24 +15,34 @@ class RenewableElectricity:
         self.hourly_cf_file = None
 
 
-    def configure(self, config_file, csv_file=None):
+    def configure(self, config=None, config_file=None, csv_file=None):
 
         """Load configuration from YAML file or use defaults or use overides from user"""
-
-        with open(config_file, 'r') as f:
-            cfg = yaml.safe_load(f)
-        self.capacity_mw = cfg['capacity_mw']
         
+        if config is not None:
+            cfg = config
+        elif config_file is not None:
+            with open(config_file, 'r') as f:
+                cfg = yaml.safe_load(f)
+        else:
+            # Default values if nothing is provided
+            cfg = {
+                'capacity_mw': 100,       # default capacity
+                'capex_per_mw': 2500,     # default CAPEX
+                'opex_per_mw': 0.03*2500  # default OPEX
+            }
+            
+
+        self.capacity_mw = cfg.get('capacity_mw',100)
         self.capex_per_mw = cfg.get('capex_per_mw', 2500)
         self.opex_per_mw = cfg.get('opex_per_mw', 0.03*2500)
+
         if csv_file is not None:
-            # Use user-provided CSV
             self.hourly_cf_file = csv_file
         else:
-            # Use default CSV from package
             with resources.path('hytea.data', 'sample_wind_cf.csv') as p:
                 self.hourly_cf_file = str(p)
-        
+
 
 
     def calculate_hourly_output(self):
@@ -43,29 +53,22 @@ class RenewableElectricity:
         self.hourly_cf = df['cf'].values
         return self.hourly_cf * self.capacity_mw
 
-
     def cumulative_energy_gwh(self, hourly=False):
-        
+
         """
         Calculate cumulative energy produced in GWh. If hourly=True, return array of cumulative GWh over time. If hourly=False, return total GWh as single number.
         
         """
+
         hourly_output = self.calculate_hourly_output()
-
-        cumulative = np.cumsum(hourly_output) / 1000  # MW -> GWh
-
-        if hourly:
-            return cumulative  # array of cumulative GWh per hour
-        else:
-            return cumulative[-1]  # total GWh, return last number in the array
-
+        cumulative = np.cumsum(hourly_output) / 1000
+        return cumulative if hourly else cumulative[-1]
 
     def average_capacity_factor(self):
-
+        
         """Calculate average capacity factor"""
 
         return self.hourly_cf.mean()
-
 
     def get_capex_opex(self):
 
@@ -75,3 +78,5 @@ class RenewableElectricity:
             'capex': self.capacity_mw * self.capex_per_mw,
             'opex': self.capacity_mw * self.opex_per_mw
         }
+
+
