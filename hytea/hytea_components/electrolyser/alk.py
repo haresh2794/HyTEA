13f86fe6d89901b0d58420f07dd787d24ccd89c8
haresh <hareshmvc@gmail.com>
2026-04-13 20:27:20 +0100

    minor reverse

--- hytea/hytea_components/electrolyser/alk.py
+++ hytea/hytea_components/electrolyser/alk.py
@@ -496,8 +496,6 @@ class ALKElectrolyser:
                 "water_m3": water_m3,
                 "efficiency": efficiency,
                 "sec_electrolyser": sec_electrolyser,
-                "sec_compressor": len(h2_hourly_kg)*[1]*self.sec_compression,
-                "sec_transport": len(h2_hourly_kg)*[1]*self.sec_transport,
                 "load": load
             }
         # ======================
