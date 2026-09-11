cce7115796d598ea030c796418c6ef36053327ac
haresh <hareshmvc@gmail.com>
2026-09-11 22:22:37 +0100

    minor change

--- hytea/utils/__init__.py
+++ hytea/utils/__init__.py
@@ -2,3 +2,4 @@ from .wind_ninja import generate_wind_cf_from_location
 from .solar_ninja import generate_solar_cf_from_location
 from .demand_profile import load_demand_profile
 from .plots_resepw import rese_plots
+from .plots_storage import storage_plots
